3bb481bd4c4cec9d982546b3ca90d73c779243dd
arianne05 <quimpoarianne5@gmail.com>
2023-11-16 12:18:46 +0800

    Added new code to fetch data in database inside views.py

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#add new import for user to fetch data from User table in a database
+from .models import User
 
 # Create your views here.
 # def index(request):
@@ -7,4 +9,7 @@ from django.http import HttpResponse
 
 #add this code to run the html templates
 def index(request):
-    return render(request, 'users/index.html')
+    user_list = User.objects.order_by('pubdate')[:5] #use negative sign before the pubdate to descend order/ [:5] limit by 5
+    context = {'user_list': user_list}
+    
+    return render(request, 'users/index.html', context) #context is added to pass the data
