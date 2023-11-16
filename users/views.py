94d43e708a56dbfbe9983d35c393a2d5b8c0f9a0
arianne05 <quimpoarianne5@gmail.com>
2023-11-16 11:34:24 +0800

    Added new function to call the html templates in users/view.py

--- users/views.py
+++ users/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# def index(request):
+#     return HttpResponse('Hello')
+
+#add this code to run the html templates
 def index(request):
-    return HttpResponse('Hello')
+    return render(request, 'users/index.html')
