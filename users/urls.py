029495c871319b109f8b1e462c86c08009c5c7e2
arianne05 <quimpoarianne5@gmail.com>
2023-11-17 11:28:46 +0800

    Added new add,html, update views and url.py

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 app_name = 'users'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('add', views.add, name='add'), #added new path for add.html
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- users/views.py
+++ users/views.py
@@ -7,9 +7,13 @@ from .models import User
 # def index(request):
 #     return HttpResponse('Hello')
 
-#add this code to run the html templates
+#add this code to run the html index.html templates
 def index(request):
     user_list = User.objects.order_by('pub_date')[:5] #use negative sign before the pubdate to descend order/ [:5] limit by 5
     context = {'user_list': user_list}
     
-    return render(request, 'users/index.html', context) #context is added to pass the data
+    return render(request, 'users/index.html', context) #context variable is added to pass its data
+
+#add this code to run the html add.html templates
+def add(request):
+    return render(request, 'users/add.html')
